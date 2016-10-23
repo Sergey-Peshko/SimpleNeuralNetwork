8ff7fb404145fe69b056e1e597f298c57b07e3b8
peshk <peshk@DESKTOP-G1VTGNL>
2016-10-23 17:37:33 +0300

    .

--- reserv copies/ActivationFunctions/IActivationFunction.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "stdafx.h"
-namespace neuralNet{
-	class IActivationFunction abstract{
-	public:
-		virtual float calculate(float S) = 0;
-		virtual float calculateFirstDerivative(float S) = 0;
-	};
-}
