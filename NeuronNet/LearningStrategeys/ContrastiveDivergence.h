6bc96304ef681a1bc5d87a6ee977f0a33ef1fc69
peshk <peshk@DESKTOP-G1VTGNL>
2016-11-11 00:01:27 +0300

    .

--- NeuronNet/LearningStrategeys/ContrastiveDivergence.h
+++ NeuronNet/LearningStrategeys/ContrastiveDivergence.h
@@ -20,15 +20,74 @@ namespace neuralNet {
 		int epochNumber = 0;
 		_logger << ("CD-k Start learning...") << std::endl;
 
-		vector<vector<float>> nablaWeights(network->HiddenLayers().size());
-		vector<float> nablaThresholdsInput(network->InputLayer());
-		vector<float> nablaThresholdsOutput(network->HiddenLayers().size());
+		vector<float> nablaThresholdsInput(network->InputLayer()->Neurons().size());
+		vector<float> nablaThresholdsOutput(network->OutputLayer()->Neurons().size());
+		vector<vector<float>> nablaWeights(network->OutputLayer()->Neurons().size(), 
+			vector<float>(network->InputLayer()->Neurons().size()));
+		
 
 		do{
-			//выполняем k итераций
 
-			//меняем синоптические связи
+			//process data set
+			int currentIndex = 0;
+			do
+			{
+				for (int i = 0; i < nablaWeights.size(); i++)
+				{
+					for (int j = 0; j < nablaWeights[i].size(); j++)
+					{
+						nablaWeights[i][j] = 0;
+					}
+					nablaThresholdsOutput[i] = 0;
+				}
+				for (int i = 0; i < nablaThresholdsInput.size(); i++) {
+					nablaThresholdsInput[i] = 0;
+				}
 
+				vector<float> startInput = data[currentIndex].Input();
+				vector<float> startOutput = network->calculateOutput(startInput);
+
+				vector<float> finishInput;
+				vector<float> finishOutput;
+
+				vector<float> prevOutput = startOutput;
+				vector<float> prevInput = startInput;
+				//выполняем k итераций
+				for (int i = 0; i < _config.getK(); i++) {
+					finishInput = network->calculateInput(prevOutput);
+					finishOutput = network->calculateOutput(finishInput);
+					//прибавляем к наблам
+					for (int neuronIndex = 0; neuronIndex < nablaWeights.size(); neuronIndex++) {
+						for (int weightIndex = 0; weightIndex < nablaWeights[neuronIndex].size(); weightIndex++) {
+							nablaWeights[neuronIndex][weightIndex] +=
+								(finishOutput[neuronIndex] - prevOutput[neuronIndex]) *
+								(finishInput[weightIndex]) *
+								network->OutputLayer()->Neurons()[neuronIndex]->ActivationFunction()->calculateFirstDerivative(
+									network->OutputLayer()->Neurons()[neuronIndex]->getLastState())
+								+
+								(finishInput[weightIndex] - prevInput[weightIndex]) *
+								(prevOutput[neuronIndex]) *
+								network->InputLayer()->Neurons()[weightIndex]->ActivationFunction()->calculateFirstDerivative(
+									network->InputLayer()->Neurons()[weightIndex]->getLastState());
+						}
+						nablaThresholdsOutput[neuronIndex] += (finishOutput[neuronIndex] - prevOutput[neuronIndex]) *
+							network->OutputLayer()->Neurons()[neuronIndex]->ActivationFunction()->calculateFirstDerivative(
+								network->OutputLayer()->Neurons()[neuronIndex]->getLastState());
+					}
+					for (int weightIndex = 0; weightIndex < nablaThresholdsInput.size(); weightIndex++) {
+						nablaThresholdsInput[weightIndex] +=
+							(finishInput[weightIndex] - prevInput[weightIndex]) *
+							network->InputLayer()->Neurons()[weightIndex]->ActivationFunction()->calculateFirstDerivative(
+								network->InputLayer()->Neurons()[weightIndex]->getLastState());
+					}
+
+					prevOutput = finishOutput;
+					prevInput = finishInput;
+				}
+				
+				//меняем синоптические связи
+
+			} while (currentIndex < data.size());
 			//вычисляем среднеквадратичную ошибку
 
 			epochNumber++;
